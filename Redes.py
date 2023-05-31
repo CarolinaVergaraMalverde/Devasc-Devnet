721c8fe4728ed236fadb16bf0f038d3871fc423b
CarolinaVergara <134557366+CarolinaVergaraMalverde@users.noreply.github.com>
2023-05-31 20:32:24 +0000

    Update Redes.py

--- Redes.py
+++ Redes.py
@@ -3,10 +3,10 @@ import requests
 
 while True:
     main_api = "https://www.mapquestapi.com/directions/v2/route?" 
-    orig = input("Origen (Ciudad de origen o Pais): ")
+    orig = input("Origen (Ciudad de origen, Pais): ")
     if orig == "S" or orig == "salida": 
         break  
-    dest = input("Destino (Ciudad de destino o Pais): ")
+    dest = input("Destino (Ciudad de destino, Pais): ")
     if dest == "S" or dest == "salida": 
         break  
     key = "MlXqQ9nqoPBKtCSS75QijE3rGNSEe9M4"  
